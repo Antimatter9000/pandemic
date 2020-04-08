02169453d76485a21226d22f1f94442ff90c9a52
antimatterjs <jonathan@antimatterdesign.com>
2020-04-08 21:59:01 +0100

    make responsive

--- src/index.js
+++ src/index.js
@@ -14,10 +14,38 @@ function start() {
     stage.animate();
 }
 
+function setupInputListener(id) {
+    document.getElementById(id).oninput = e => {
+        document.getElementById(`${id}-value`).textContent = e.target.value;
+    }
+}
+
 if (!window.location.search.includes('hideControls=true')) {
     document.getElementById('controls').classList.remove('hidden');
     document.getElementById('results').classList.remove('hidden');
 }
 
+
+setupInputListener('initial-population');
+setupInputListener('speed');
+setupInputListener('infection-rate');
+setupInputListener('mortality-rate');
+setupInputListener('severity');
+setupInputListener('asymptomatic-period');
+setupInputListener('symptomatic-period');
+
+document.getElementById('controls-toggle').onclick = () => {
+    document.getElementById('results').classList.remove('visible');
+    document.getElementById('controls').classList.toggle('visible');
+}
+
+document.getElementById('results-toggle').onclick = () => {
+    document.getElementById('results').classList.toggle('visible');
+    document.getElementById('controls').classList.remove('visible');
+}
+
 start();
-document.getElementById('start-button').onclick = start;
+document.getElementById('start-button').onclick = () => {
+    document.getElementById('controls').classList.remove('visible');
+    start();
+}
